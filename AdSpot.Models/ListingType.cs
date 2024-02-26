89271c27ca8910cc53c6fc3d48250d7fd533c1d8
Matthew Sia <matthewsia98@gmail.com>
2024-02-26 14:09:36 -0500

    format

--- AdSpot.Models/ListingType.cs
+++ AdSpot.Models/ListingType.cs
@@ -1,6 +1,7 @@
 namespace AdSpot.Models;
 
-public class ListingType {
+public class ListingType
+{
     public int Id { get; set; }
     public string Name { get; set; }
 
--- AdSpot.Models/Platform.cs
+++ AdSpot.Models/Platform.cs
@@ -1,6 +1,7 @@
 namespace AdSpot.Models;
 
-public class Platform {
+public class Platform
+{
     public int PlatformId { get; set; }
     public string Name { get; set; }
     public ICollection<ListingType> ListingTypes { get; set; }
