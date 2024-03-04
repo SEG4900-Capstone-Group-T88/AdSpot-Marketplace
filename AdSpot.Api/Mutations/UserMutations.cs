0f0cf7d2c5b8cc46ae11c6ec9676d531ecd85e33
Damian Oportus <coportus99@gmail.com>
2024-03-04 13:52:22 -0500

    Update and get user

--- AdSpot.Api/Mutations/UserMutations.cs
+++ AdSpot.Api/Mutations/UserMutations.cs
@@ -14,6 +14,24 @@ public class UserMutations
         return user;
     }
 
+    public User GetUser(int userId, UserRepository repo)
+        {
+            var user = repo.GetUser(userId);
+            return user;
+        }
+
+        public User UpdateUser(int userId, string email, string password, UserRepository repo)
+            {
+                var user = repo.UpdateUser(new User
+                {
+                    Id = userId,
+                    Email = email,
+                    Password = password
+                });
+
+                return user;
+            }
+
     public int DeleteUser(int userId, UserRepository repo) {
         var deletedId = repo.DeleteUser(userId);
         return deletedId;
