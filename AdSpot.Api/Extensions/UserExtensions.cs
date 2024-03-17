6d449f7bd8663350df6e044b8a3733583010bdc8
Matthew Sia <matthewsia98@gmail.com>
2024-03-17 11:25:36 -0400

    add queries to get orders

--- AdSpot.Api/Extensions/UserExtensions.cs
+++ AdSpot.Api/Extensions/UserExtensions.cs
@@ -3,16 +3,5 @@
 [ExtendObjectType<User>]
 public class UserExtensions
 {
-    [UseProjection]
-    public IQueryable<Order> GetPendingRequests([Parent] User user, OrderRepository repo)
-    {
-        return repo.GetPendingRequests(user.UserId);
-    }
 
-    [UseProjection]
-    [UseFiltering]
-    public IQueryable<Order> GetPurchases([Parent] User user, OrderRepository repo)
-    {
-        return repo.GetAllPurchases(user.UserId);
-    }
 }
--- AdSpot.Api/Queries/OrderQueries.cs
+++ AdSpot.Api/Queries/OrderQueries.cs
@@ -16,4 +16,46 @@ public class OrderQueries
     {
         return repo.GetOrderById(orderId);
     }
+
+    [UseProjection]
+    [UseSorting]
+    public IQueryable<Order> GetPendingRequests(int userId, OrderRepository repo)
+    {
+        return repo.GetPendingRequests(userId);
+    }
+
+    [UseProjection]
+    [UseSorting]
+    public IQueryable<Order> GetAcceptedRequests(int userId, OrderRepository repo)
+    {
+        return repo.GetAcceptedRequests(userId);
+    }
+
+    [UseProjection]
+    [UseSorting]
+    public IQueryable<Order> GetRejectedRequests(int userId, OrderRepository repo)
+    {
+        return repo.GetRejectedRequests(userId);
+    }
+
+    [UseProjection]
+    [UseSorting]
+    public IQueryable<Order> GetPendingOrders(int userId, OrderRepository repo)
+    {
+        return repo.GetPendingOrders(userId);
+    }
+
+    [UseProjection]
+    [UseSorting]
+    public IQueryable<Order> GetAcceptedOrders(int userId, OrderRepository repo)
+    {
+        return repo.GetAcceptedOrders(userId);
+    }
+
+    [UseProjection]
+    [UseSorting]
+    public IQueryable<Order> GetRejectedOrders(int userId, OrderRepository repo)
+    {
+        return repo.GetRejectedOrders(userId);
+    }
 }
