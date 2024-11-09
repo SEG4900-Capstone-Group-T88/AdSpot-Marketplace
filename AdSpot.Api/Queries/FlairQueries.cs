ad8fa636da7324e4079bb4746e3f97f0b220f268
Akarsh Gharge <akarshgharge20@gmail.com>
2024-11-09 18:10:25 -0500

    Added Get Flairs query

---
+++ AdSpot.Api/Queries/FlairQueries.cs
@@ -0,0 +1,14 @@
+namespace AdSpot.Api.Queries;
+
+[QueryType]
+public class FlairQueries
+{
+    [Authorize]
+    public IQueryable<Flair> GetFlairs (
+        int userId,
+        FlairRepository flairRepo
+    )
+    {
+        return flairRepo.GetFlairs(userId);
+    }
+}
